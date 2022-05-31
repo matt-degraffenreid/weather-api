fb73f77b21b972bb886117dbf673c536308a7899
Matthew DeGraffenreid <matthew@degraffenreid.io>
2022-05-31 02:58:37 -0600

    adding functional test

---
+++ src/functional-test/java/org/cresta/weather/EndpointTest.java
@@ -0,0 +1,48 @@
+package org.cresta.weather;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.cresta.weather.domain.WeatherRequest;
+import org.cresta.weather.repository.Weather;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = App.class)
+public class EndpointTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void shouldReturnWeatherData() throws JsonProcessingException {
+        WeatherRequest weatherRequest = WeatherRequest.builder()
+                .user("John Doe")
+                .location("Denver")
+                .build();
+        ResponseEntity<String> response = restTemplate.postForEntity("/weather/hello", weatherRequest, String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void shouldReturnElapsedTime() throws JsonProcessingException {
+        WeatherRequest weatherRequest = WeatherRequest.builder()
+                .user("John Doe")
+                .location("Denver")
+                .build();
+        restTemplate.postForEntity("/weather/hello", weatherRequest, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity("/weather/goodbye", weatherRequest, String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
