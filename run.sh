3b8c9b99ba48abfdad6e2e50cd8671fd9f647247
Matthew DeGraffenreid <matthew@degraffenreid.io>
2022-06-07 10:57:15 -0600

    new deploy

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
+cd /home/ec2-user/cresta
 aws s3 cp s3://cresta-bucket-app/app.sh .
-chmod +X app.sh
-./app.sh
-cd /home/ec2-user/cresta && ./gradlew build
+chmod +x app.sh
+./app.sh && ./gradlew build
 nohup java -jar build/libs/cresta-weather-java.jar > log.log 2>&1 &
 
