7f721dabc3698f2597e9fcdb17dee69f6d196560
Matthew DeGraffenreid <matthew@degraffenreid.io>
2022-06-07 08:11:20 -0600

    changing to bootrun

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 cd /home/ec2-user/cresta
-chmod +x gradlew
-./gradlew build && java -jar build/libs/cresta-weather-java.jar
-curl "localhost:8080/weather/hello" -X POST -H "Content-Type: application/json" -d '{"user": "123456", "location": "denver"}'
+./gradelew bootrun
