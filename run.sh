02a0b37cd1a3b983c2eb7e605f12750634917dd7
Matthew DeGraffenreid <matthew@degraffenreid.io>
2022-06-07 07:45:56 -0600

    adding new server port

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 cd /home/ec2-user/cresta
 chmod +x gradlew
 ./gradlew build && java -jar build/libs/cresta-weather-java.jar
+curl "localhost:8080/weather/hello" -X POST -H "Content-Type: application/json" -d '{"user": "123456", "location": "denver"}'
