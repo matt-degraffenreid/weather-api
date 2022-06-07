626d5b97323f38e95bed543c6fe2ea23dfce2a51
Matthew DeGraffenreid <matthew@degraffenreid.io>
2022-06-07 10:54:22 -0600

    new deploy

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-export GOOGLE_API_KEY={{ssm:GoogleAPIKey}}
-export OPEN_WEATHER_API_KEY={{ssm:WeatherAPIKey}}
+aws s3 cp s3://cresta-bucket-app/app.sh .
+chmod +X app.sh
+./app.sh
 cd /home/ec2-user/cresta && ./gradlew build
 nohup java -jar build/libs/cresta-weather-java.jar > log.log 2>&1 &
 
