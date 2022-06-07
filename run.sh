e5bc070d9c54ae50d30ce16791c6618374a555e0
Matthew DeGraffenreid <matthew@degraffenreid.io>
2022-06-07 07:31:37 -0600

    redoing run file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 cd /home/ec2-user/cresta
-docker build -t bootdocker:6 .
-docker run -d --name bootdocker -p 8080:8080 bootdocker:6
+chmod +x gradlew
+./gradlew build && java -jar build/libs/cresta-weather-java.jar
