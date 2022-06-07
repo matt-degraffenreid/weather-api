889c31c3d3171a389c6dd199b1878926ca0dab12
Matthew DeGraffenreid <matthew@degraffenreid.io>
2022-06-07 10:29:03 -0600

    fixing appspec

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 export GOOGLE_API_KEY={{ssm:GoogleAPIKey}}
 export OPEN_WEATHER_API_KEY={{ssm:WeatherAPIKey}}
-cd /home/ec2-user/cresta && ./gradlew bootrun
+cd /home/ec2-user/cresta && ./gradlew build
+nohup java -jar build/libs/cresta-weather-java.jar > log.log 2>&1 &
 
